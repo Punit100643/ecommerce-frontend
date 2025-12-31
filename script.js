19541fad601ea5f14fe4dcf009eaa385a5deb48e
Punit100643 <ps0477580@gmail.com>
2025-12-31 03:55:41 +0000

    final

--- script.js
+++ script.js
@@ -1,3 +1,78 @@
+
+// ---------- CART LOGIC ----------
+
+// Get cart from localStorage
+function getCart() {
+  return JSON.parse(localStorage.getItem("cart")) || [];
+}
+
+// Save cart
+function saveCart(cart) {
+  localStorage.setItem("cart", JSON.stringify(cart));
+  updateCartCount();
+}
+
+// Add to cart (USED IN product.html)
+function addToCart(id, name, price, image) {
+  const product = {
+    id: id,
+    name: name,
+    price: Number(price),
+    image: image,
+    quantity: 1
+  };
+
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  const existingProduct = cart.find(item => item.id === id);
+
+  if (existingProduct) {
+    existingProduct.quantity += 1;
+  } else {
+    cart.push(product);
+  }
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+  updateCartCount();
+
+  alert("Product added to cart!");
+}
+
+
+
+// Update cart count in navbar
+function updateCartCount() {
+  const cart = getCart();
+ const count = cart.reduce((sum, item) => {
+  return sum + (Number(item.quantity) || 0);
+}, 0);
+
+
+  const cartCount = document.getElementById("cart-count");
+  if (cartCount) {
+    cartCount.innerText = count;
+  }
+}
+
+// Run on page load
+document.addEventListener("DOMContentLoaded", updateCartCount);
+
+    // Size selection
+const sizeButtons = document.querySelectorAll(".sizes button");
+
+sizeButtons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    // remove active from all
+    sizeButtons.forEach(b => b.classList.remove("active"));
+
+    // add active to clicked one
+    btn.classList.add("active");
+
+    // optional: store selected size
+    const selectedSize = btn.innerText;
+    console.log("Selected Size:", selectedSize);
+  });
+});
 // ================= COUNTDOWN TIMER =================
 const endTime = new Date().getTime() + 8 * 60 * 60 * 1000;
 
